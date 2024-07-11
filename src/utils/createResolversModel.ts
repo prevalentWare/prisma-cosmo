742fcc928f172354c318b8a462b96b4bd00720cb
jhon gil <jagil@prevalentware.com>
2024-07-11 08:47:09 -0500

    Build: Se agregan resolvers y types desde general con queries de firmado para s3

--- src/utils/createResolversModel.ts
+++ src/utils/createResolversModel.ts
@@ -168,5 +168,38 @@ const createResolvers = async (model: GQLModel, parsedModels: GQLModel[]) => {
     ),
     resolverFile
   );
+
+  const generalResolverFile = `  
+    import { Resolver } from '@/types';
+    import { getSignedUrlForUpload } from '@/utils/getSignedURL';
+    import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+    import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+
+    const generalResolvers: Resolver = {
+      Query: {
+        getSignedUrlForUpload: async (parent, args) => {
+          return await {
+            fileName: args.file,
+            url: getSignedUrlForUpload(args.file),
+          };
+        },
+        getMultipleSignedUrlsForUpload: async (parent, args) => {
+          return await Promise.all(
+            args.files.map(async (file: string) => ({
+              fileName: file,
+              url: await getSignedUrlForUpload(file),
+            }))
+          );
+        },
+      },
+      Mutation: {},
+    };
+    export { generalResolvers };
+`;
+
+writeFile(
+  path.join(process.cwd(), `prisma/generated/models/general/resolvers.ts`),
+  generalResolverFile
+);
 };
 export { createResolvers };
